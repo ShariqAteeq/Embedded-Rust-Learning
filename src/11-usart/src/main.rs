b055f69fe0875f2607ce8e7ae06f7efb43e3762a
= <=>
2020-02-02 15:31:50 +0500

    run USART and send a string through serial communication with minicom

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -11,8 +11,14 @@ fn main() -> ! {
 
     // Send a single character
     //.bits method is uded to transfer all data
-    //tdr is a register to store data
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    //tdr is a register to store
+   // usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+
+   // Send a string
+   for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+    usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+}
+
 
     loop {}
 }
