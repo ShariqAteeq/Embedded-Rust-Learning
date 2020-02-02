212463c02c6f439e2c6407a9549bcb64791f3d01
= <=>
2020-02-02 16:22:22 +0500

    to find time b/w transmitting data on itmdump

--- src/11-usart/src/main.rs
+++ src/11-usart/src/main.rs
@@ -7,18 +7,21 @@ use aux11::{entry, iprint, iprintln};
 
 #[entry]
 fn main() -> ! {
-    let (usart1, mono_timer, itm) = aux11::init();
+    let (usart1, mono_timer, mut itm) = aux11::init();
 
-    // Send a single character
-    //.bits method is uded to transfer all data
-    //tdr is a register to store
-   // usart1.tdr.write(|w| w.tdr().bits(u16::from(b'X')));
+    let instant = mono_timer.now();
+    // Send a string
+    for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
+        usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
+    }
+    let elapsed = instant.elapsed(); // in ticks
 
-   // Send a string
-   for byte in b"The quick brown fox jumps over the lazy dog.".iter() {
-    usart1.tdr.write(|w| w.tdr().bits(u16::from(*byte)));
-}
-
+    iprintln!(
+        &mut itm.stim[0],
+        "`for` loop took {} ticks ({} us)",
+        elapsed,
+        elapsed as f32 / mono_timer.frequency().0 as f32 * 1e6
+    );
 
     loop {}
 }
